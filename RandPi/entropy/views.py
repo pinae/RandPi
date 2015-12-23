09cb92bb3bfd7c760ed876720ecc79ae6a632df0
pinae <jonny@pinae.net>
2015-12-23 15:12:58 +0100

    Angular2 app

--- RandPi/entropy/views.py
+++ RandPi/entropy/views.py
@@ -127,11 +127,15 @@ def index(request):
     return HttpResponse("""<!DOCTYPE html>
     <html>
       <head>
-        <script src="entropy/static/angular2.min.js"></script>
-        <script src="entropy/static/main.js"></script>
+        <meta charset="UTF-8">
+        <script src="entropy/static/angular2-polyfills.js"></script>
+        <script src="entropy/static/Rx.umd.js"></script>
+        <script src="entropy/static/angular2-all.umd.dev.js"></script>
+        <script src="entropy/static/app.component.js"></script>
+        <script src="entropy/static/boot.js"></script>
       </head>
       <body>
-        <my-app></my-app>
+        <my-app>Loading ...</my-app>
       </body>
     </html>""", content_type='text/html')
 
